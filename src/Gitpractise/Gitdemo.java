ab73a6c0f02e570036f3244344a07f6dce914cd3
nemmojuchanti <nemmoju.chanti@gmail.com>
2019-09-21 17:19:30 +0530

    chanti commit

---
+++ src/Gitpractise/Gitdemo.java
@@ -0,0 +1,12 @@
+package Gitpractise;
+
+public class Gitdemo
+{
+public static void main(String[] args) {
+	System.out.println("hello");
+	
+	
+	System.out.println("helllo hai");
+	System.out.println("whats up man");
+}
+}

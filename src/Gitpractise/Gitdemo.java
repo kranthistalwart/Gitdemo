12000fa535c40508b1b3f4b0f3a597b064acfdfe
kranthistalwart <kranthistalwart143@gmail.com>
2019-09-22 11:43:58 +0530

    new update by Ramakrishna333

--- src/Gitpractise/Gitdemo.java
+++ src/Gitpractise/Gitdemo.java
@@ -4,12 +4,11 @@ public class Gitdemo
 {
 public static void main(String[] args) {
 	System.out.println("hello");
-
 	System.out.println("chanti");
-
 	System.out.println("hello");
-
 	System.out.println("chanti");
 	System.out.println("kranthi");
+	System.out.println("succesfully completed git practise");
+	
 }
 }
